5b2af9202b27ad08a23bf0a878c212b53a7887c0
Kushtrim Duraki <kd31844@seeu.edu.mk>
2026-02-09 07:48:23 +0100

    Fix Tailwind production build

--- JobPortal.Web/src/main.jsx
+++ JobPortal.Web/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { WishlistProvider } from "./context/WishlistContext";
+import "./styles/app.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
